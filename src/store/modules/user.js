593dbbf5a470a7412186d511e7d38b1fe9d05428
sasonhe <642139009@qq.com>
2019-05-17 18:29:15 +0800

    ojj

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -27,10 +27,8 @@ const actions = {
     return new Promise((resolve, reject) => {
       login({ username: username.trim(), password: password }).then(response => {
         const { data } = response
-        // commit('SET_TOKEN', data.token)
-        // setToken(data.token)
-        console.log(response)
-        console.log(data)
+        commit('SET_TOKEN', data.token)
+        setToken(data.token)
         resolve()
       }).catch(error => {
         reject(error)
